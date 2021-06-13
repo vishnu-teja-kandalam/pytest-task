f9a442adfc0b09bfa6106018b0079658ba64a566
vishnu.k <vishnu.k@yxiom.com>
2021-06-13 19:51:16 +0530

    one

--- test_cases.py
+++ test_cases.py
@@ -1,18 +1,20 @@
-import pytest
 from basicmath.util import add_num, sub_num, mul_num, div_num
 
 
 class TestClass:
+    ''' Class for testing four operations'''
     def test_function_add(self):
+        '''method for addition'''
         assert add_num(10,20) == 30
 
     def test_function_sub(self):
+        '''method for substraction'''
         assert sub_num(20,10) == 10
 
     def test_function_mul(self):
+        '''method for multiplication'''
         assert mul_num(2,2)==4
 
     def test_function_div(self):
-        with pytest.raises(ZeroDivisionError):
-            div_num(5,0)
-
+        '''method for division'''
+        assert div_num(15,5)==3
